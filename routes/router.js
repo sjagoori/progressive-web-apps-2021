764e2914421afe16dbd0cf57f4bda42792219afb
sjagoori <sjagoori@users.noreply.github.com>
2021-03-16 12:46:11 +0100

    + update path

--- routes/router.js
+++ routes/router.js
@@ -1,36 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const getData = require('../modules/api.js')
-const renderData = getDataset()
+const api = require('../modules/api.js')
+const cache = require('../modules/cache.js')
+const path = require('path');
 
-router.get('/:coin*?', async (res, req) => {
-  return res.params.coin ? req.render('detailpage', { query: { data: await getCoinData(res.params.coin) } }) : req.render('homepage', { query: { title: "Hello world", data: await renderData } });;
+router.get('/:subreddit*?', async (req, res) => {
+  return req.params.subreddit ? res.render('detailpage', await api.getSubredditData(req.params.subreddit.split(".").slice(0, -1).join("."))) : res.render('homepage', JSON.parse(await cache.getCache('subreddits')))
 })
 
 module.exports = router;
-
-
-async function getDataset() {
-  let header = {
-    headers: {
-      "X-Requested-With": "lolthismaybeanything"
-    }
-  }
-
-  let topList = await getData("https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=12&CMC_PRO_API_KEY=" + process.env.API_KEY, header)
-  let infoList = await getData('https://shacors.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=' + topList.data.map(item => item.id).toString() + '&CMC_PRO_API_KEY=' + process.env.API_KEY, header)
-
-  let merged = Object.values(infoList.data).map(key => {
-    return key.quote = Object.values(topList.data)
-      .map(item => item.name == key.name ? item.quote.USD : false)
-      .filter(item => typeof item === 'object')
-  })
-
-  let renderData = ({ ...infoList.data, merged }, delete infoList.data.merged) ? Object.values(infoList.data).sort((a, b) => b.quote[0].market_cap - a.quote[0].market_cap) : null
-  return renderData
-}
-
-async function getCoinData(pick) {
-  return Object.values(await renderData).map(key => key.name == pick ? key : false).filter(item => typeof item === 'object')[0]
-}
