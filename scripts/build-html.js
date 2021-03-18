2b77188d2aae293bc994ccd91b452e70232c5fde
sjagoori <sjagoori@users.noreply.github.com>
2021-03-18 21:29:54 +0100

    + optional writeToFile function for params to SW

--- scripts/build-html.js
+++ scripts/build-html.js
@@ -6,7 +6,6 @@ const fsPromises = fs.promises;
 const path = require('path')
 const cache = require('../modules/cache.js')
 
-// cache.setCache('subreddits', JSON.stringify({ data: [{ title: 'gaming' }, { title: 'funny' }, { title: 'music' }, { title: 'pics' }, { title: 'science' }]}))
 generateHomepage()
 generateDetailpage()
 
@@ -14,16 +13,20 @@ async function generateHomepage() {
   const data = JSON.parse(await cache.getCache('subreddits'))
   const html = renderTemplate('./views/homepage.ejs', data)
   writeFile('./dist', 'index.html', html)
+  return;
+  // This looks as stupid as eval() but is it really stupid if it works? God bless hoisting tho
+  // writeToFile('\nconst cacheElements = JSON.parse(' + JSON.stringify(data)+')')
 }
 
 async function generateDetailpage() {
   const statics = JSON.parse(await cache.getCache('subreddits')).data.map(key => key.title)
 
-  statics.forEach( async (element) => {
+  statics.forEach(async (element) => {
     const data = JSON.parse(await cache.getCache(element))
     const html = renderTemplate('./views/detailpage.ejs', data)
     writeFile('./dist', element + '.html', html)
   });
+  return;
 }
 
 function renderTemplate(templatePath, data) {
@@ -34,4 +37,10 @@ function renderTemplate(templatePath, data) {
 async function writeFile(fileDirectory, filename, fileContents) {
   await fsPromises.mkdir(fileDirectory, { recursive: true });
   return await fsPromises.writeFile(path.join(fileDirectory, filename), fileContents);
+}
+
+async function writeToFile(text) {
+  fd = fs.openSync(path.join(__dirname, '../static', 'service-worker.js'), 'a')
+  fs.writeSync(fd, text)
+  fs.closeSync(fd)
 }
