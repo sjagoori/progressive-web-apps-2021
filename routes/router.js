372e1097d1176f54baaa4704155895cd6604e59e
sjagoori <sjagoori@users.noreply.github.com>
2021-03-20 22:11:39 +0100

    + improved conditions

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,18 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const api = require('../modules/api.js')
-const cache = require('../modules/cache.js')
-const path = require('path');
+const api = require("../modules/api.js");
+const cache = require("../modules/cache.js");
 
-router.get('/:subreddit*?', async (req, res) => {
-  return req.params.subreddit ? res.render('detailpage', await api.getSubredditData(req.params.subreddit.split(".").slice(0, -1).join("."))) : res.render('homepage', {data: cache.getCache('subreddits')})
-})
+router.get("/:subreddit*?", async (req, res) => {
+  let param = req.params.subreddit ? req.params.subreddit.endsWith(".html")
+    ? req.params.subreddit.split(".").slice(0, -1).join(".")
+    : req.params.subreddit
+    : null
+
+    return req.params.subreddit
+    ? res.render("detailpage", await api.getSubredditData(param))
+    : res.render("homepage", { data: cache.getCache("subreddits") });
+});
 
 module.exports = router;
