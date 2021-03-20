79c69a91d1da799415fcb9431d663a012219f3db
sjagoori <sjagoori@users.noreply.github.com>
2021-03-20 22:11:04 +0100

    ~ formatting

--- modules/api.js
+++ modules/api.js
@@ -1,5 +1,5 @@
-const axios = require('axios')
-const cache = require('../modules/cache.js')
+const axios = require("axios");
+const cache = require("../modules/cache.js");
 
 /**
  * Function makes an API calls and caches them if they're not cached before
@@ -7,32 +7,35 @@ const cache = require('../modules/cache.js')
  * @param {Object} header Header params
  */
 async function getSubredditData(subreddit) {
-  return cache.getCache(subreddit) ? await cache.getCache(subreddit) : await fetchData(subreddit)
+  return cache.getCache(subreddit)
+    ? await cache.getCache(subreddit)
+    : await fetchData(subreddit);
 }
 
 module.exports.getSubredditData = getSubredditData;
 
 async function fetchData(subreddit) {
-  return await axios.get("https://www.reddit.com/r/" + subreddit + ".json", { headers: { "X-Requested-With": "lolthismaybeanything" } })
-    .then(response => {
-      return response.data.data.children.length == 0 ? {
-        timestamp: +new Date,
-        title: "Not found",
-        data: [],
-      } :
-        (
-          cache.setCache(subreddit, {
-            timestamp: +new Date,
-            title: 'r/' + subreddit,
-            data: response.data.data.children,
-          })
-          ,
-          {
-            timestamp: +new Date,
-            title: 'r/' + subreddit,
-            data: response.data.data.children,
-          }
-        )
+  return await axios
+    .get("https://www.reddit.com/r/" + subreddit + ".json", {
+      headers: { "X-Requested-With": "lolthismaybeanything" },
     })
-    .catch(error => console.log(error.status))
+    .then((response) => {
+      return response.data.data.children.length == 0
+        ? {
+            timestamp: +new Date(),
+            title: "Not found",
+            data: [],
+          }
+        : (cache.setCache(subreddit, {
+            timestamp: +new Date(),
+            title: "r/" + subreddit,
+            data: response.data.data.children,
+          }),
+          {
+            timestamp: +new Date(),
+            title: "r/" + subreddit,
+            data: response.data.data.children,
+          });
+    })
+    .catch((error) => console.log(error.status));
 }
