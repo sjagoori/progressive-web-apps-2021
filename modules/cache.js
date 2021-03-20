34d591617336503786e8b27663bc101118e10b46
sjagoori <sjagoori@users.noreply.github.com>
2021-03-20 21:33:31 +0100

    + cache functions

--- modules/cache.js
+++ modules/cache.js
@@ -1,68 +1,41 @@
 const asyncRedis = require("async-redis");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = low(adapter);
 
-/**
- * Function sets key:value pair
- * @param {String} key key
- * @param {String} value value
- * @returns status code
- */
-function setCache(key, value) {
-  console.log(key, value);
-  const client = asyncRedis.createClient({
-    host: process.env.REDIS_URL,
-    port: process.env.REDIS_PORT
-  });
+if (!db.has("subreddits").value())
+  db.defaults({
+    subreddits: [
+      {
+        title: "gaming",
+      },
+      {
+        title: "funny",
+      },
+      {
+        title: "music",
+      },
+      {
+        title: "pics",
+      },
+      {
+        title: "science",
+      },
+    ],
+  }).write();
 
-  return (client.set(key, value))
+// setCache('gaming', {title: 'gaming'})
+async function setCache(subreddit, value) {
+  db.set(subreddit, value).write();
 }
 
 module.exports.setCache = setCache;
 
-/**
- * Function gets key:value pair
- * @param {String} key key
- * @returns value of key
- */
-async function getCache(key) {
-  const client = asyncRedis.createClient({
-    host: process.env.REDIS_URL,
-    port: process.env.REDIS_PORT
-  });
+// console.log(getCache('subreddits'))
 
-  return (await client.get(key))
+function getCache(subreddit) {
+  return db.get(subreddit).value();
 }
 
 module.exports.getCache = getCache;
-
-/**
- * Function gets values for multiple keys
- * @param {Object} keys object of keys
- * @returns object of values
- */
-async function getMultiple(keys) {
-  const client = asyncRedis.createClient({
-    host: process.env.REDIS_URL,
-    port: process.env.REDIS_PORT
-  });
-
-  return client.mget(keys)
-}
-
-module.exports.getMultiple = getMultiple;
-
-/**
- * Function deletes key:value pair
- * @param {String} key key
- * @returns status code
- */
-function deleteCache(key) {
-  const client = asyncRedis.createClient({
-    host: process.env.REDIS_URL,
-    port: process.env.REDIS_PORT
-  });
-
-  return (client.del(key))
-}
-
-module.exports.deleteCache = deleteCache;
-
