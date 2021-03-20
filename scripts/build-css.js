54cc432f16a5af8f96ffb3a86fc9e86d7c41697e
sjagoori <sjagoori@users.noreply.github.com>
2021-03-20 21:36:39 +0100

    ~ naming

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -1,14 +1,27 @@
-const gulp = require('gulp')
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
 
-return gulp.src([
-  './static/css/main.css'
-])
-.pipe(concat('main.css'))
-.pipe(cleanCSS())
-.pipe(autoprefixer({
-  cascade: false
-}))
-.pipe(gulp.dest('./dist/css'))
+return (
+  gulp
+    .src(["./static/css/detailpage.css"])
+    .pipe(concat("detailpage.css"))
+    .pipe(cleanCSS())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest("./dist/css")),
+  gulp
+    .src(["./static/css/homepage.css"])
+    .pipe(concat("homepage.css"))
+    .pipe(cleanCSS())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest("./dist/css"))
+);
