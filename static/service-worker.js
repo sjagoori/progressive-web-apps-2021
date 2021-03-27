01828d0796fb2c68617d8b3821d0ad70281c3c0a
sjagoori <sjagoori@users.noreply.github.com>
2021-03-27 17:30:15 +0100

    + offline support

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,3 +1,4 @@
+const offlinePage = 'offline.html'
 const cacheName = 'cache-v1'
 const cacheURLs = [
   './funny.html',
@@ -5,8 +6,12 @@ const cacheURLs = [
   './index.html',
   './music.html',
   './pics.html',
-  './askreddit.html'
+  './askreddit.html',
+  './offline.html',
+  './css/homepage.css',
+  './css/detailpage.css'
 ]
+
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js', {
@@ -14,49 +19,81 @@ if ('serviceWorker' in navigator) {
   }).then(function (registration) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    initialCache();
   }, function (err) {
     // registration failed :(
     console.log('ServiceWorker registration failed: ', err);
   });
 }
 
-self.addEventListener('install', async (e) =>{
+async function initialCache() {
+  console.log('init')
   const cache = await caches.open(cacheName)
-  await cache.open(cacheURLs)
-  return self.skipWaiting()
-})
 
-
-self.addEventListener('activate', e =>{
-  self.clients.claim()
-})
-
-self.addEventListener('fetch', async (e) =>{
-  const req = e.request;
-  const url = new URL(req.url)
-
-  if (url.orgin == location.origin){
-    e.respondWith(cacheFirst(req))
-  } else {
-    e.respondWith(networkAndCache(req))
-  }
-})
-
-async function cacheFirst(req){
-  const cache = await caches.open(cacheName)
-  const cached = await cache.match(req)
-  return cached || fetch(req)
+  cacheURLs.map(key => cache.add(key));
 }
 
-async function networkAndCache(req){
-  const cache = await caches.open(cacheName)
-  try{
-    const fresh = await fetch(req)
-    await cache.put(req, fresh.clone())
-    return fresh
-  }catch (e){
-    const cached = await cache.match(req)
-    return cached
-  }
+self.ononline = function () {
+  console.log('Your worker is now online');
 }
+
+self.onoffline = function (e) {
+  console.log('Your worker is now offline');
+}
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
+      }).catch(error => {
+        return caches.match(offlinePage);
+      })
+  );
+});
+
+
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+  self.skipWaiting();
+})
+
+// self.addEventListener('fetch', async (e) =>{
+//   const req = e.request;
+//   const url = new URL(req.url)
+
+//   const cache = await caches.open(cacheName)
+//   cache.addAll(cacheURLs)
+
+//   if (url.orgin == location.origin){
+//     e.respondWith(cacheFirst(req))
+//   } else {
+//     e.respondWith(networkAndCache(req))
+//   }
+// })
+
+// async function cacheFirst(req){
+//   const cache = await caches.open(cacheName)
+//   const cached = await cache.match(req)
+//   return cached || fetch(req)
+// }
+
+// async function networkAndCache(req){
+//   const cache = await caches.open(cacheName)
+//   try{
+//     const fresh = await fetch(req)
+//     await cache.put(req, fresh.clone())
+//     return fresh
+//   }catch (e){
+//     const cached = await cache.match(req)
+//     return cached
+//   }
+// }
 
