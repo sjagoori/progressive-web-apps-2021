5f024306f53112058223a0829205f52b96a22e2b
sjagoori <sjagoori@users.noreply.github.com>
2021-03-28 20:57:13 +0200

    + compression

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const port = process.env.PORT || 3000;
 const router = require('./routes/router');
 const express = require('express');
 const app = express();
+const compression = require('compression')
 
-// Setting EJS as template engine 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
-
-// We're using this file as router for all routes
+app.use(compression())
 app.use('/', router);
 
-// Listen to port
 app.listen(port);
