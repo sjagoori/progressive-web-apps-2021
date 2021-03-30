f865f18f4609298dd4c2e68eea05bace42a0360c
sjagoori <sjagoori@users.noreply.github.com>
2021-03-30 14:59:10 +0200

    + docs

--- modules/cache.js
+++ modules/cache.js
@@ -3,12 +3,22 @@ const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
 
+/**
+ * Function set key:value pair
+ * @param {String} subreddit - key
+ * @param {Object} value - value
+ */
 async function setCache(subreddit, value) {
   db.set(subreddit, value).write();
 }
 
 module.exports.setCache = setCache;
 
+/**
+ * Function gets value for given key
+ * @param {String} subreddit - key
+ * @returns {Object} key's value
+ */
 function getCache(subreddit) {
   return db.has(subreddit).value() ? db.get(subreddit).value() : undefined;
 }
