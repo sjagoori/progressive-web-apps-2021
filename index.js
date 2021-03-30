a6d1b75a7c8aff80f376a202f369cebd28c97037
sjagoori <sjagoori@users.noreply.github.com>
2021-03-30 11:15:47 +0200

    + minify & compress

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const router = require('./routes/router');
 const express = require('express');
 const app = express();
 const compression = require('compression')
+const minify = require('express-minify');
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/static'));
 app.use(compression())
+app.use(minify());
+app.use(express.static(__dirname + '/static'));
 app.use('/', router);
 
 app.listen(port);
