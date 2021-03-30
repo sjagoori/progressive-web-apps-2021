58c9dfefee86b9b5d78b149e8a20efcc782810cd
sjagoori <sjagoori@users.noreply.github.com>
2021-03-30 14:55:54 +0200

    - formatting & cleanup

--- static/service-worker.js
+++ static/service-worker.js
@@ -15,16 +15,13 @@ const cacheURLs = [
   './scripts/main.js'
 ]
 
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js', {
-    scope: '.' // <--- THIS BIT IS REQUIRED
+    scope: '.'
   }).then(function (registration) {
-    // Registration was successful
     console.log('ServiceWorker registration successful with scope: ', registration.scope);
     initialCache();
   }, function (err) {
-    // registration failed :(
     console.log('ServiceWorker registration failed: ', err);
   });
 }
@@ -50,10 +47,8 @@ self.addEventListener('fetch', event => {
     caches.match(event.request)
       .then(response => {
         if (response) {
-          // console.log('Found ', event.request.url, ' in cache');
           return response;
         }
-        // console.log('Network request for ', event.request.url);
         return fetch(event.request)
       }).catch(error => {
         return caches.match(offlinePage);
@@ -61,42 +56,7 @@ self.addEventListener('fetch', event => {
   );
 });
 
-
-
 self.addEventListener('activate', e => {
   self.clients.claim();
   self.skipWaiting();
 })
-
-// self.addEventListener('fetch', async (e) =>{
-//   const req = e.request;
-//   const url = new URL(req.url)
-
-//   const cache = await caches.open(cacheName)
-//   cache.addAll(cacheURLs)
-
-//   if (url.orgin == location.origin){
-//     e.respondWith(cacheFirst(req))
-//   } else {
-//     e.respondWith(networkAndCache(req))
-//   }
-// })
-
-// async function cacheFirst(req){
-//   const cache = await caches.open(cacheName)
-//   const cached = await cache.match(req)
-//   return cached || fetch(req)
-// }
-
-// async function networkAndCache(req){
-//   const cache = await caches.open(cacheName)
-//   try{
-//     const fresh = await fetch(req)
-//     await cache.put(req, fresh.clone())
-//     return fresh
-//   }catch (e){
-//     const cached = await cache.match(req)
-//     return cached
-//   }
-// }
-
